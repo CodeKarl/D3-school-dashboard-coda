0f6fbd5b749e40a491a2baf1ee2b317927ed454e
KarelReitsma <reitsma.karel@gmail.com>
2018-10-19 14:11:23 +0200

    Made a seperate data.js file that contains the student data and linked it in index.html

---
+++ js/data.js
@@ -0,0 +1,12 @@
+var data = [
+    {name: "Ana",       rate: 9,        comment:"Very intensive but challenging. I love it"},
+    {name: "Mike",      rate: 5,        comment:"It is too hard for me and I struggle understand the lessons"},
+    {name: "Jacob",     rate: 10,       comment:"I love the academy! I learn so many things!"},
+    {name: "Sil",       rate: 7,        comment:"It is cool I expected more challenges"},
+    {name: "Vanya",     rate: 8,        comment:"The lessons are very organized and it’s easy to keep up."},
+    {name: "David",     rate: 3,        comment:"I couldn t understand anything from the first day. I would like more lessons than working by myself"},
+    {name: "Karel",     rate: 10,       comment:"Best academy ever! So challenging and exciting"},
+    {name: "Jiri",      rate: 9,        comment:"I love the teachers and the challenges."},
+    {name: "Kate",      rate: 9,        comment:"I am satisfied with the level  intensity."},
+    {name: "Jane",      rate: 9,        comment:"It is exactly what I expected a great academy"},
+];
